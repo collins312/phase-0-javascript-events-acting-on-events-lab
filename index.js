996fceb8bee0ad2ca6dc2b0a1860f5effb31580f
collins otieno <collins.ouma2@student.moringaschool.com>
2025-06-27 18:51:55 +0300

    java

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+
+function moveDodgerLeft() {
+  let left = dodger.style.left.replace('px', '');
+  left = parseInt(left, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = dodger.style.left.replace('px', '');
+  left = parseInt(left, 10);
+
+  if (left < 360) { // assuming container is 400px and dodger is 40px wide
+    dodger.style.left = `${left + 1}px`;
+  }
+}
